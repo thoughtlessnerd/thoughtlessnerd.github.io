67bfd4e9199809700765ff6470cca8b4b93a2d83
thoughtlessnerd <abhay.csgo001@gmail.com>
2023-04-08 15:11:07 +0530

    hash routing working

--- src/App.tsx
+++ src/App.tsx
@@ -8,19 +8,20 @@ import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
 import "./assets/fonts/Gotham-Font/Gotham-Bold.otf";
 import "./assets/fonts/Gotham-Font/Gotham-Light.otf";
+import { HashRouter } from "react-router-dom";
 
 const App: React.FC = () => {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/About" element={<About />} />
-                <Route path="/Contact" element={<Contact />} />
-                <Route path="/Blogs" element={<Blogs />} />
-                <Route path="/Projects" element={<Projects />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <HashRouter basename="/">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/Blogs" element={<Blogs />} />
+        <Route path="/Projects" element={<Projects />} />
+      </Routes>
+    </HashRouter>
+  );
 };
 
 export default App;
--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -3,47 +3,47 @@ import { Menu, menuItem } from "./Menu";
 import "./Nav.css";
 
 const Nav: React.FC = () => {
-    const menuList: menuItem[] = [
-        {
-            label: "Home",
-            href: "/",
-        },
-        {
-            label: "Projects",
-            href: "/Projects/",
-        },
-        {
-            label: "About",
-            href: "/About/",
-        },
-        {
-            label: "Blogs",
-            href: "/Blogs/",
-        },
-        {
-            label: "Contact me",
-            href: "/Contact/",
-        },
-    ];
+  const menuList: menuItem[] = [
+    {
+      label: "Home",
+      href: "/#",
+    },
+    {
+      label: "Projects",
+      href: "/#/Projects",
+    },
+    {
+      label: "About",
+      href: "/#/About",
+    },
+    {
+      label: "Blogs",
+      href: "/#/Blogs",
+    },
+    {
+      label: "Contact me",
+      href: "/#/Contact",
+    },
+  ];
 
-    let activeIdx: number | undefined = undefined;
-    const address: string = document.location.href.slice(
-        document.location.origin.length
-    );
-    console.log(address);
-    for (let i: number = 0; i < menuList.length; i++) {
-        if (address === menuList[i].href) {
-            activeIdx = i;
-            break;
-        }
+  let activeIdx: number | undefined = undefined;
+  const address: string = document.location.href.slice(
+    document.location.origin.length
+  );
+  console.log(address);
+  for (let i: number = 0; i < menuList.length; i++) {
+    if (address === menuList[i].href) {
+      activeIdx = i;
+      break;
     }
+  }
 
-    return (
-        <nav>
-            <Logo nameFirst="Thoughtless" nameSecond="Nerd" />
-            <Menu list={menuList} activeItem={activeIdx}></Menu>
-        </nav>
-    );
+  return (
+    <nav>
+      <Logo nameFirst="Thoughtless" nameSecond="Nerd" />
+      <Menu list={menuList} activeItem={activeIdx}></Menu>
+    </nav>
+  );
 };
 
 export default Nav;
