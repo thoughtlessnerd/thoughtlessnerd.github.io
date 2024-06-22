70ca9971137e13738a3e61bd5d7e023a309e4d92
thoughtlessnerd <abhay.csgo001@gmail.com>
2024-06-22 16:22:11 +0530

    name and title for some basic stuff

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ function App() {
   return (
     <>
       <NoisyBg />
+      <section className="w-full min-h-[100vh] flex flex-col gap-3 justify-center items-center absolute top-0 left-0">
+        <h1 className="text-6xl font-bold styled-font">Abhay Chauhan</h1>
+        <p>Software Developer</p>
+      </section>
     </>
   );
 }
