b01343492406aed106d672d880b8ebe4e36bd5d9
thoughtlessnerd <abhay.csgo001@gmail.com>
2024-06-22 13:45:36 +0530

    Tailwind and fonts setup with vite + react framework

---
+++ README.md
@@ -0,0 +1,3 @@
+# Abhay Chauhan's Portfolio
+
+Here I will try to showcase all my skills.
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='styled-font text-6xl'>Abhay Chauhan</h1>
+      <p>Software Developer</p>
+    </>
+  )
+}
+
+export default App
